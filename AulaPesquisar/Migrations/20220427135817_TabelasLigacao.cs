132d3c916e3d2eb74645ba18c71be59b714f26ed
Fernando Notebook <fcgraciano@gmail.com>
2022-04-27 11:00:37 -0300

    aula

---
+++ AulaPesquisar/Migrations/20220427135817_TabelasLigacao.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AulaPesquisar.Migrations
+{
+    public partial class TabelasLigacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "permissao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permissao", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PESSOAS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    idade = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cpf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    rg = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    data_nasc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    sexo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    mae = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pai = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cep = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    endereco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    numero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    bairro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cidade = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    telefone_fixo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    celular = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PESSOAS", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NomeCompleto = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "permissao_usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    PermissaoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permissao_usuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_permissao_usuario_permissao_PermissaoId",
+                        column: x => x.PermissaoId,
+                        principalTable: "permissao",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_permissao_usuario_usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permissao_usuario_PermissaoId",
+                table: "permissao_usuario",
+                column: "PermissaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permissao_usuario_UsuarioId",
+                table: "permissao_usuario",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "permissao_usuario");
+
+            migrationBuilder.DropTable(
+                name: "PESSOAS");
+
+            migrationBuilder.DropTable(
+                name: "permissao");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
